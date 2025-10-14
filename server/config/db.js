451be57511b875102fc219354dad86174e8f75bb
TalhaRehman-KHT <talhakhank51@gmail.com>
2025-10-14 15:13:11 +0500

    added Mongoose url

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,20 @@
 // config/db.js
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // load env variables
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
         console.log("✅ MongoDB Connected Successfully");
     } catch (err) {
         console.error("❌ MongoDB Connection Failed:", err.message);
-        process.exit(1);
+        process.exit(1); // exit process with failure
     }
 };
 
